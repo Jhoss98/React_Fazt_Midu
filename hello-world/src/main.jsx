02aa3cda929c747362a87da92af8b9549ee0238c
Jhoss Pedraza <jhosser@live.com>
2023-04-19 23:08:17 -0500

    the new React Project by Fazt

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Greating, UserCard } from "./Greating";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <Greating />
+    <UserCard></UserCard>
+  </>
+);
